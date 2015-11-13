f5434fec2897693c6c370fa25143aa1d6ff87e1c
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2015-11-13 11:50:40 -0300

    add constructor, categoria, getter and setter to Fila Class'

--- BarbeariaZero/src/Fila.java
+++ BarbeariaZero/src/Fila.java
@@ -15,12 +15,29 @@ public class Fila
 {
   
   ConcurrentLinkedQueue<Militar> queue;
-   
+  private int categoria;
+
+  public Fila(int categoria)
+  {
+    this.categoria = categoria;
+    queue = new ConcurrentLinkedQueue<>();
+  }
+
+  public int getCategoria()
+  {
+    return categoria;
+  }
+
+  public void setCategoria(int categoria)
+  {
+    this.categoria = categoria;
+  }
+  
   public ConcurrentLinkedQueue<Militar> getQueue()
   {
     return queue;
   }
-
+  
   public void setQueue(ConcurrentLinkedQueue<Militar> queue)
   {
     this.queue = queue;
