46a4276d8ab3bcf141cdcc5e11909079b33c36ff
Ronaldo de Figueiredo Silveira <ronaldof1s1@gmail.com>
2015-11-13 10:26:21 -0300

    Created a ConcurrentLinkedQueue in Barbearia

--- BarbeariaZero/src/Barbearia.java
+++ BarbeariaZero/src/Barbearia.java
@@ -3,15 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.concurrent.*;
+import java.lang.Thread;
 /**
  *
  * @author ronaldofs
  */
 public class Barbearia
 {
-  public static void main(String args[])
+  public static void main(String args[]) throws InterruptedException
   {
+    
+    ConcurrentLinkedQueue<Militar> q1;
+    q1 = new ConcurrentLinkedQueue<>();
+    Thread.sleep(4000);
+    
     System.out.println("OI");
   }
 }
