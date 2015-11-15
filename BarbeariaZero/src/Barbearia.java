74456f3b305f13248ff580e5d86f084d6ec8ccad
Luis Tertulino da Cunha Neto <luistertulino@localhost.localdomain>
2015-11-15 00:01:28 -0300

    method for generate the input data

--- BarbeariaZero/src/Barbearia.java
+++ BarbeariaZero/src/Barbearia.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 import java.lang.Thread;
+import java.util.Random;
 /**
  *
  * @author ronaldofs
@@ -26,7 +27,45 @@ public class Barbearia
   public static Barbeiro barbeiro_sargento;
   public static Barbeiro barbeiro_praca;
   
-  
+  public static int input[][];
+
+  public void generate_input(){
+	input = new int[1000][2];
+
+	Random rand = new Random();
+
+	/* 
+		Gera aleatoriamente a entrada de dados.
+		Ao todos, são 1000 registros.
+		Como espefificado, a barbearia deve fechar após cinco pausas (representadas pelo zero) consecutivas.
+		Por isso, apenas os 995 primeiros registros são gerados de forma aleatória; os outros cinco serão 
+			explicitamente definidos como zero.
+	*/
+
+	for (int i = 0; i < 995; i++) {
+		input[i][0] = rand.nextInt(4);
+
+		if (input[i][0] == 1) { // Oficial
+			input[i][1] = rand.nextInt(3) + 4;
+		}
+		else if (input[i][0] == 2) { // Sargento
+			input[i][1] = rand.nextInt(3) + 2;
+		}
+		else if (input[i][0] == 3) { // Praça
+			input[i][1] = rand.nextInt(3) + 1;
+		}
+		else {
+			input[i][1] = 0;
+		}
+	}
+
+	input[995][0] = 0; input[995][1] = 0;
+	input[996][0] = 0; input[996][1] = 0;
+	input[997][0] = 0; input[997][1] = 0;
+	input[998][0] = 0; input[998][1] = 0;
+	input[999][0] = 0; input[999][1] = 0;
+	
+  }
   
   public static void main(String args[]) throws InterruptedException
   {
